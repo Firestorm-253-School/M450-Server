6dfd117b63376c186a8b2ef0900ad36d20547fcf
isabelge <isabel.geissmann@bbz-sh.ch>
2026-09-22 07:36:58 +0200

    add highscore

--- src/snake_game_server/app.py
+++ src/snake_game_server/app.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 
 from .api.players import router as players_router
 from .sockets.game_socket import router as socket_router
+from .api.highscores import router as highscores_router
 
 
 app = FastAPI()
 
 app.include_router(players_router)
 app.include_router(socket_router)
+
+app.include_router(highscores_router)
